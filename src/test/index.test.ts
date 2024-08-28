a9a4aecdb7ffae13213a05f5eb001635466ce3e8
lafkpages <luisafk@proton.me>
2024-08-28 11:58:02 +0200

    test: unit tests for all APIs

--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -1,6 +1,16 @@
-import { expect, test } from "bun:test";
+import { describe, expect, test } from "bun:test";
 
-import { isInFlight } from "..";
+import {
+  getConnectivityStatus,
+  getDeviceStatus,
+  getFlightData,
+  getFlightStatus,
+  getFlightTrajectory,
+  getHealthReport,
+  getInformationalMessages,
+  getPortalMetadata,
+  isInFlight,
+} from "..";
 import { isInFlight as isInFlightNode } from "../node";
 
 const isReallyInFlight = confirm("Are you currently in an Air France flight?");
@@ -8,4 +18,38 @@ const isReallyInFlight = confirm("Are you currently in an Air France flight?");
 test("isInFlight", () => {
   expect(isInFlight()).resolves.toBe(isReallyInFlight);
   expect(isInFlightNode()).resolves.toBe(isReallyInFlight);
+});
+
+describe.if(isReallyInFlight)("in-flight APIs", () => {
+  test("getPortalMetadata", () => {
+    expect(getPortalMetadata()).resolves.toBeObject();
+  });
+
+  test("getFlightStatus", () => {
+    expect(getFlightStatus()).resolves.toBeObject();
+  });
+
+  test("getConnectivityStatus", () => {
+    expect(getConnectivityStatus()).resolves.toBeObject();
+  });
+
+  test("getHealthReport", () => {
+    expect(getHealthReport()).resolves.toBeArray();
+  });
+
+  test("getFlightTrajectory", () => {
+    expect(getFlightTrajectory()).resolves.toBeObject();
+  });
+
+  test("getInformationalMessages", () => {
+    expect(getInformationalMessages()).resolves.toBeObject();
+  });
+
+  test("getDeviceStatus", () => {
+    expect(getDeviceStatus()).resolves.toBeObject();
+  });
+
+  test("getFlightData", () => {
+    expect(getFlightData()).resolves.toBeObject();
+  });
 });
